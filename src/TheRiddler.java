1c4d96bd0f6337155d663a962a4b1504f0e44e49
League <league@WTS9.attlocal.net>
2018-02-11 17:16:53 -0800

    Revert "Modual 1"
    This reverts commit e83fe39ef1a31fca30809f014cf0d5cd04de54c7.

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,5 +1,3 @@
-import javax.swing.JOptionPane;
-
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -10,20 +8,17 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-int score = 100;
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-String answer = JOptionPane.showInputDialog("What has a head and a tail, but no body?");
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-if (answer.equals("a coin")) {JOptionPane.showMessageDialog(null, "Correct!");
-score=score+10;}
-		// 5. Otherwise, say "wrong" and tell them the answer
-else { JOptionPane.showMessageDialog(null, "WRONG"); score=score-10;
 
-}
+		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+
+		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+
+		// 5. Otherwise, say "wrong" and tell them the answer
+
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		JOptionPane.showMessageDialog(null,score);
+		
 	}
 }
 
