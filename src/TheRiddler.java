e83fe39ef1a31fca30809f014cf0d5cd04de54c7
League <league@WTS9.attlocal.net>
2018-02-11 17:13:52 -0800

    Modual 1

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,17 +10,20 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 100;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String answer = JOptionPane.showInputDialog("What has a head and a tail, but no body?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (answer.equals("a coin")) {JOptionPane.showMessageDialog(null, "Correct!");
+score=score+10;}
 		// 5. Otherwise, say "wrong" and tell them the answer
+else { JOptionPane.showMessageDialog(null, "WRONG"); score=score-10;
 
+}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null,score);
 	}
 }
 
